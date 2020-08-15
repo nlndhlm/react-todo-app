fe5a9fcbb443de625a96b8f50094e5f17915a5ee
Nicolai <nicolai.lindholm10@gmail.com>
2020-08-15 14:27:01 +0200

    Updates

---
+++ precache-manifest.c57bcfbf7553abbd8aa628c4b9503c30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "057468f78c408b104add896d876630df",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "ceec8fed967d8d00308c",
+    "url": "/react-todo-app/static/css/main.9a5dc838.chunk.css"
+  },
+  {
+    "revision": "0e681452f0009672ea48",
+    "url": "/react-todo-app/static/js/2.0744a038.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.0744a038.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceec8fed967d8d00308c",
+    "url": "/react-todo-app/static/js/main.acf8d0b4.chunk.js"
+  },
+  {
+    "revision": "1528fb5b7daed31266f4",
+    "url": "/react-todo-app/static/js/runtime-main.1dd0cb46.js"
+  }
+]);
